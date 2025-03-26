7593153af207873a1419076edd2628686137982d
kimdaewhi <kimdaewhi@naver.com>
2025-03-26 17:02:51 +0900

    볼린저 밴드 점수 계산 함수 추가 및 호출 방식 static method로로 수정

--- strategies/sma_crossover.py
+++ strategies/sma_crossover.py
@@ -32,6 +32,23 @@ def RSI(values, window=14):
     return 100 - (100 / (1 + rs))   # RSI 계산
 
 
+@staticmethod
+def calculate_bb_score_z(current_price, bb_mid, bb_upper, bb_lower, num_std=2):
+    """볼린저 밴드 점수 계산"""
+    """ current_price: 현재 가격 """
+    """ bb_upper: 볼린저 밴드 상단 """
+    """ bb_lower: 볼린저 밴드 하단 """
+    """ z-score를 이용한 점수 계산 """
+    """ 최종적으로 산출된 z-score와 B.B 가중치를 곱하여 -3 ~ 3점으로 변환 """
+    bb_width = bb_upper - bb_lower
+    if bb_width == 0:
+        return 0
+    
+    std = bb_width / (2 * num_std) # 표준편차 계산
+    z = (current_price - bb_mid) / std
+
+    return float(np.clip(z * 3, -3, 3)) # 과매도 최대 3점, 과매수 최소 -3점
+
 
 class SmaBollingerStrategy(Strategy):
     n1 = 20  # 단기 이동평균 기간(1개월)
@@ -69,25 +86,9 @@ class SmaBollingerStrategy(Strategy):
 
 
         # 2. 볼린저 밴드 점수 계산(가중치 30%)
-        bb_score = self.calculate_bb_score_z(self.data.Close[-1], self.bb_mid[-1], self.bb_upper[-1], self.bb_lower[-1])
+        bb_score = calculate_bb_score_z(self.data.Close[-1], self.bb_mid[-1], self.bb_upper[-1], self.bb_lower[-1])
         score += bb_score * self.bb_weight
 
-    
-    def calculate_bb_score_z(current_price, bb_mid, bb_upper, bb_lower, num_std=2):
-        """볼린저 밴드 점수 계산"""
-        """ current_price: 현재 가격 """
-        """ bb_upper: 볼린저 밴드 상단 """
-        """ bb_lower: 볼린저 밴드 하단 """
-        """ z-score를 이용한 점수 계산 """
-        """ 최종적으로 산출된 z-score와 B.B 가중치를 곱하여 -3 ~ 3점으로 변환 """
-        bb_width = bb_upper - bb_lower
-        if bb_width == 0:
-            return 0
-        
-        std = bb_width / (2 * num_std) # 표준편차 계산
-        z = (current_price - bb_mid) / std
-
-        return float(np.clip(z * 3, -3, 3)) # 과매도 최대 3점, 과매수 최소 -3점
     
     
     
